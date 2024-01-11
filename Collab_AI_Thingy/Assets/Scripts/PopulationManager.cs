f6316c32b3f1e270790056cdbbbaa780b4fba21c
DakotaRo <142523799+DakotaRo@users.noreply.github.com>
2024-01-11 13:35:07 -0600

    I don't even remember doing anything...

--- Collab_AI_Thingy/Assets/Scripts/PopulationManager.cs
+++ Collab_AI_Thingy/Assets/Scripts/PopulationManager.cs
@@ -1,37 +1,102 @@
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
 using System.Linq;
 
-public class Populationmanager : MonoBehaviour
-{
-    public GameObject botPrefab;
-    public GameObject[] startingPos;
-    public int populationSize = 50;
-    List,GameObject> population = new List<GameObject>();
-    public static float elapsed = 0;
-    public float trialTime = 10;
-    public float timeScale = 2;
-    int generation = 1;
-    
-    GUIStyle guiStyle = new GUIStyle();
-    void OnGUI()
-    {
-        guiStyle.fontSize = 25;
-        guiStyle.normal.textColor = Color.white;
-        GUI.BeginGroup (new Rect (10, 10, 250, 150));
-        GUI.Box (new Rect (0,0,140,140), "Stats", guiStyle);
-        GUI.Label(new Rect (10,25,200,30), "Gen: " + generation, guiStyle);
-        GUI.Label(new Rect (10,50,200,30), string.Format("Time: {0:0.00}",elapsed), guiStyle);
-        GUI.Label(new Rect (10,75,200,30), "Population: " + population.Count, guiStyle);
-        GUI.EndGroup ();
-    }
+public class PopulationManager : MonoBehaviour {
 
-    void Start () 
-    {
-        for(int i = 0; i < populationSize; i++)
-        {
-            int starti = Random.Range(0, startingPos.Length);
-            GameObject b = Instantiate(botPrefab, startingPos[starti].transform.position, this.t)
-        }
-    }
+	public GameObject botPrefab;
+	public GameObject[] startingPos;
+	public int populationSize = 50;
+	List<GameObject> population = new List<GameObject>();
+	public static float elapsed = 0;
+	public float trialTime = 10;
+	public float timeScale = 2;
+	int generation = 1;
+
+	GUIStyle guiStyle = new GUIStyle();
+	void OnGUI()
+	{
+		guiStyle.fontSize = 25;
+		guiStyle.normal.textColor = Color.white;
+		GUI.BeginGroup (new Rect (10, 10, 250, 150));
+		GUI.Box (new Rect (0,0,140,140), "Stats", guiStyle);
+		GUI.Label(new Rect (10,25,200,30), "Gen: " + generation, guiStyle);
+		GUI.Label(new Rect (10,50,200,30), string.Format("Time: {0:0.00}",elapsed), guiStyle);
+		GUI.Label(new Rect (10,75,200,30), "Population: " + population.Count, guiStyle);
+		GUI.EndGroup ();
+	}
+
+
+	// Use this for initialization
+	void Start () {
+		for(int i = 0; i < populationSize; i++)
+		{
+			int starti = Random.Range(0, startingPos.Length);
+			GameObject b = Instantiate(botPrefab, startingPos[starti].transform.position, this.transform.rotation);
+			b.transform.Rotate(0, Mathf.Round(Random.Range(-90, 91) / 90) * 90, 0);
+			population.Add(b);
+		}
+		Time.timeScale = timeScale;
+	}
+
+	GameObject Breed(GameObject parent1, GameObject parent2)
+	{
+		int starti = Random.Range(0, startingPos.Length);
+		GameObject offspring = Instantiate(botPrefab, startingPos[starti].transform.position, this.transform.rotation);
+		offspring.transform.Rotate(0, Mathf.Round(Random.Range(-90, 91) / 90) * 90, 0);
+		Brain b = offspring.GetComponent<Brain>();
+		if(Random.Range(0, 100) == 1)
+		{
+			b.Init();
+		}
+		else
+		{
+			b.Init();
+			b.dna.Combine(parent1.GetComponent<Brain>().dna, parent2.GetComponent<Brain>().dna);
+		}
+		return offspring;
+	}
+
+	void BreedNewPopulation()
+	{
+		List<GameObject> sortedList = population.OrderByDescending(o => o.GetComponent<Brain>().offenseBlocked).ToList();
+		string offenseBlocked = "Generation: " + generation;
+		foreach(GameObject g in sortedList)
+		{
+			offenseBlocked += ", " + g.GetComponent<Brain>().offenseBlocked;
+		}
+		Debug.Log("Offense: " + offenseBlocked);
+		population.Clear();
+
+		while(population.Count < populationSize)
+		{
+			int bestParentCutoff = sortedList.Count / 2;
+			for(int i = 0; i < bestParentCutoff -1; i++)
+			{
+				for(int j = 1; j < bestParentCutoff; j++)
+				{
+					population.Add(Breed(sortedList[i], sortedList[j]));
+					if(population.Count == populationSize) break;
+					population.Add(Breed(sortedList[j], sortedList[i]));
+					if(population.Count == populationSize) break;
+				}
+				if(population.Count == populationSize) break;
+			}
+		}
+		for(int i = 0; i < sortedList.Count; i++)
+		{
+			Destroy(sortedList[i]);
+		}
+		generation++;
+	}
+	
+	void Update ()
+	{
+		elapsed += Time.deltaTime;
+		if(elapsed >= trialTime)
+		{
+			BreedNewPopulation();
+			elapsed = 0;
+		}
+	}
 }
